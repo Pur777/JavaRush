29fc4fbb94b2045a8804098b9467198812530239
Pur777 <46963306+Pur777@users.noreply.github.com>
2019-03-25 09:30:47 +0300

    1

---
+++ 4.JavaCollections/src/com/javarush/task/task36/task3601/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task36.task3601;
+
+/* 
+MVC - простая версия
+*/
+public class Solution {
+    View view = new View();
+    public static void main(String[] args) {
+        new Solution().view.fireShowDataEvent();
+    }
+
+}
