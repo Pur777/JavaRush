2ff3ed70d1e860eb17e0d044dddf8041cdd52b6b
Pur777 <46963306+Pur777@users.noreply.github.com>
2019-04-16 13:49:06 +0300

    1

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public static void print() {}
+    public static void print(String s) {}
+    public static void print(int a) {}
+    public static void print(int a, String s) {}
+    public static void print(String s, int a) {}
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1217/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.task12.task1217;
+
+/* 
+Лететь, бежать и плыть
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    public interface CanFly {
+        void Fly();
+    }
+    public interface CanRun {
+        void Run(int a);
+    }
+    public interface CanSwim {
+        void Swim();
+    }
+
+}
